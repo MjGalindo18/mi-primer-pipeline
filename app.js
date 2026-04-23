1f045ed690ee91a4ca87103415af4a0958ab82eb
MjGalindo18 <163310342+MjGalindo18@users.noreply.github.com>
2026-04-23 21:53:44 +0000

    feat: cambiar mensaje a Hola Arquitectos

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+const http = require('http');
+
 function sumar(a, b) {
-  return a + b; 
+  return a + b;
 }
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hola Arquitectos! 2 + 3 = ' + sumar(2, 3));
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log('Servidor corriendo en puerto ' + PORT);
+});
 
 module.exports = { sumar };
